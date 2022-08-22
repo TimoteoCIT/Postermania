77199a185e669788bdd19d6fb544d957281cd098
Timoteo Rider <Timoteo.Rider@ctpcit.ed.cr>
2022-08-22 12:15:14 -0600

    Update Home

--- Postermania/Controllers/HomeController.cs
+++ Postermania/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Postermania.DAL;
+using Postermania.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +8,27 @@ using System.Web.Mvc;
 
 namespace Postermania.Controllers
 {
+    public class HomeView
+    {
+        public List<Poster> Posters { get; set; }
+        public List<Poster> Scrolls { get; set; }
+        public List<Poster> Frameds { get; set; }
+    }
+
     public class HomeController : Controller
     {
+        private PosterManiaContext db = new PosterManiaContext();
+
         public ActionResult Index()
         {
-            return View();
+            var homeView = new HomeView()
+            {
+                Posters = db.Posters.Where(x => x.Type == ItemType.Poster).Take(5).ToList(),
+                Scrolls = db.Posters.Where(x => x.Type == ItemType.Scroll).Take(5).ToList(),
+                Frameds = db.Posters.Where(x => x.Type == ItemType.Framed).Take(5).ToList()
+            };
+
+            return View(homeView);
         }
 
         public ActionResult About()
